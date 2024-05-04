531719d333fca2562b8a07a8977d5275f77baa38
idsta.ivandi <idsta.ivandi@xl.co.id>
2024-05-04 21:37:35 +0700

    add shadcn lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
+const Home = () => {
+
+  const userName: string = "ivandjoh";
+
   return (
-    <div>Hello World</div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <Button variant={"outline"}>Hello, {userName}</Button>
+      </div>
+    </main>
   );
 }
+
+export default Home;
